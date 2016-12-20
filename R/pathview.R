36928a30264eed5ab64f07d4077a5874c104c368
askarbek-orakov <askarbek.orakov@nu.edu.kz>
2016-12-20 11:08:15 +0900

    a

--- R/pathview.R
+++ R/pathview.R
@@ -11,24 +11,54 @@ getAllAnnots <- function(file = mdt) {
   ind
 }
 
+#'Plots graphs for single specific species
 #'
-#'
-#'Takes in Filename of metagenome sample annotation data to create formatted "anno" file. 
-#'@param file filename containing anotation of your sample
+#'@details 1) gets annoations of reads for single species in "d" file
+#'@details 2) splits rows with several "md5"s  to separate rows with unique "m5" values in "d5" file
+#'@details 3) confers unique rows resulted from merging of "d5" and "d.kres" files at "m5" and "md5" columns respectively. "d.kres" file adds up kegg orthology IDs by each unique "m5" value
+#'@details 4) "adk5" file has enrichments on each KEGG Orthology ID for each metagenome sample
+#'@details 5) "gdk5" has the same information as "adk5", but for 5 samples only.
+#'@param 
 #'@return formatted annotation file for metagenome sample annotation from input file
 #'@export
-mergeAnnots<-function(f,s){
-  keycols<-names(f)[1:12]
-  setkeyv(f,keycols)
-  setkeyv(s,keycols)
-  d.merge<-merge(f,s,all=TRUE,suffixes = c('.fun','.sp'))
-  names(d.merge)<-gsub('semicolon separated list of annotations.','',names(d.merge))
-  return(d.merge)  
+extractFigures <- function(SpecieName) {
+  d<-getSpecieFromAbundMD5(d.bm,sp = SpecieName,aggregate = FALSE)
+  d5<-d[,list(m5=unlist(str_split(md5,',')),mgm4714659.3,mgm4714661.3,mgm4714663.3,mgm4714665.3,mgm4714667.3,mgm4714669.3,mgm4714671.3,mgm4714673.3,mgm4714675.3,mgm4714677.3,mgm4714679.3),by=.(usp,ufun,md5)]
+  dk5<-unique(merge(d5,d.kres,all=FALSE,by.x='m5',by.y='md5')[,.(m5,usp,ufun,mgm4714659.3,mgm4714661.3,mgm4714663.3,mgm4714665.3,mgm4714667.3,mgm4714669.3,mgm4714671.3,mgm4714673.3,mgm4714675.3,mgm4714677.3,mgm4714679.3,ko)])
+  adk5<-aggregate(.~ko,as.data.frame(dk5[,-c(1:3)]),FUN=sum)
+  rownames(adk5)<-adk5$ko
+  gdk5<-adk5[,2:6]
+  names(gdk5)<-c('MFC1_p','MFC3_p','MFC1_a','MFC3_a','inflowSW')
+  gdk5[,5]<-adk5[,1+which(mdt$MFCID==names(gdk5)[5])]
+  for(i in 1:4) gdk5[,i]<-rowSums(adk5[,1+which(mdt$MFCID==names(gdk5)[i])])
+  
+  
+  for(pid in pwlist){
+    pv.out <- pathview(gene.data = adk5[,c(1,8,3,10)+1], pathway.id = pid,
+                       species = "ko", out.suffix = paste0(SpecieName,".ko.plankton"), kegg.native = T,
+                       limit = list(gene=range(as.vector(as.matrix(adk5[,c(1,8,3,10)+1]))),cpd=1))
+    pv.out <- pathview(gene.data = log2(adk5[,c(1,8,3,10)+1]+1), pathway.id = pid,
+                       species = "ko", out.suffix = paste0(SpecieName,".log2.ko.plankton"), kegg.native = T,
+                       limit = list(gene=range(as.vector(as.matrix(log2(adk5[,c(1,8,3,10)+1]+1)))),cpd=1))
+    pv.out <- pathview(gene.data = adk5[,c(4,11,9,6)+1], pathway.id = pid,
+                       species = "ko", out.suffix = paste0(SpecieName,".ko.anode"), kegg.native = T,
+                       limit = list(gene=range(as.vector(as.matrix(adk5[,c(4,11,9,6)+1]))),cpd=1))
+    pv.out <- pathview(gene.data = log2(adk5[,c(4,11,9,6)+1]+1), pathway.id = pid,
+                       species = "ko", out.suffix = paste0(SpecieName,".log2.ko.anode"), kegg.native = T,
+                       limit = list(gene=range(as.vector(as.matrix(log2(adk5[,c(4,11,9,6)+1]+1)))),cpd=1))
+    pv.out <- pathview(gene.data = gdk5, pathway.id = pid,
+                       species = "ko", out.suffix = paste0(SpecieName,".ko"), kegg.native = T,
+                       limit = list(gene=range(as.vector(as.matrix(gdk5)))))
+    pv.out <- pathview(gene.data = log2(gdk5+1), pathway.id = pid,
+                       species = "ko", out.suffix = paste0(SpecieName,".log2.ko"), kegg.native = T,
+                       limit = list(gene=range(as.vector(as.matrix(log2(gdk5+1))))))
+  }
+  obj<-as.matrix(d[,-(1:3)])
+  rownames(obj)<-d$ufun
+  colnames(obj)<-mdt$MGN
+  res<-plotHeatmap(obj,100,trace = "none", col = heatmapCols,main='Nitrosomonas functions, \nnorm and log-transformed')
+  showTable(res,'Nitrosomonas')
+  plotHeatmap(obj,100,norm = FALSE,trace = "none", col = heatmapCols,main='Nitrosomonas functions \nlog-transformed')
+  plotSP(d.bm[,-3],sp = 'Nitrosomonas')
+  
 }
-
-#'Analysis of maches
-#'
-#'Creates a list called "mlist" that contains functional and taxanomical analysis of one metagenome (mgm4714659.*). 
-#'@param file filename containing anotation of your sample
-#'@return formatted annotation file for metagenome sample annotation from input file
-#'@export
