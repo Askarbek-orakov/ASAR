f4a1928e8fd0f75d722e90b16130ae1a6f60ce60
Nazgul Sakenova <nazguls@GoryaninUnitMacBookPro.local>
2017-05-16 15:56:59 +0900

    aaaa
    Former-commit-id: 645b6e3a20163815d3d9aa955f4491b74f45bbf8

--- API download.R
+++ API download.R
@@ -1,1 +1,83 @@
-curl  -H "auth: K8qdy8tr2FfgxuU83thKceAJt" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714675.3?source=SEED&type=organism&identity=60&length=15" -o mgm4714675.3.seed
+curl  -H "auth: y8MLy5NRMBfCRGzPcxYP9b86c" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714675.3?source=SEED&type=organism&identity=60&length=15" -o mgm4714675.3.seed
+ibv5dnQkteDnpbsWU5YJj3JKH
+http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714659.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH
+
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714661.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714661.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714663.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714663.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714665.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714665.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714667.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714667.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714669.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714669.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714671.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714671.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714673.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714673.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714675.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714675.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714677.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714677.3.seed"
+Invoke-WebRequest -Uri "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4714679.3?source=SEED&type=organism&identity=60&length=15&auth=ibv5dnQkteDnpbsWU5YJj3JKH" -OutFile "mgm4714679.3.seed"
+
+http://api.metagenomics.anl.gov/annotation/similarity/mgm4685151.3?type=function&source=SEED&identity=60&length=15&auth=mgrast%20ibv5dnQkteDnpbsWU5YJj3JKH&browser=1
+in file mgm4685151 (1).3
+
+GO_EL_06_S3_L001_R1_001              mgm4685151.3
+GO_electricigens_1_S1_L001_R2_001    mgm4673324.3
+GO_electricigeus_2_S2_L001_R1_001    mgm4675922.3
+GO_electricigeus_2_S2_L001_R2_001    mgm4675923.3
+GO_EL_10_S7_L001_R1_001              mgm4685116.3
+GO_EL_10_S7_L001_R2_001              mgm4685117.3
+GO_EL_09_S6_L001_R1_001              mgm4685127.3
+GO_EL_09_S6_L001_R2_001              mgm4685128.3
+GO_EL_08_S5_L001_R2_001              mgm4685129.3
+GO_EL_08_S5_L001_R1_001              mgm4685130.3
+GO_electricigens_1_S1_L001_R1_001    mgm4673326.3
+GO_EL_07_S4_L001_R1_001              mgm4685152.3
+GO_EL_04_S1_L001_R2_001              mgm4685153.3
+GO_EL_07_S4_L001_R2_001              mgm4685154.3
+GO_EL_04_S1_L001_R1_001              mgm4685155.3
+GO_EL_05_S2_L001_R1_001              mgm4685156.3
+GO_EL_06_S3_L001_R2_001              mgm4685157.3 
+GO_EL_05_S2_L001_R2_001              mgm4685158.3
+GO_electricigens_3_S3_L001_R2_001    mgm4690995.3
+GO_electricigens_3_S3_L001_R1_001    mgm4690996.3
+
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685151.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685151.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4673324.3?source=SEED&type=organism&identity=60&length=15" -o mgm4673324.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4675922.3?source=SEED&type=organism&identity=60&length=15" -o mgm4675922.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4675923.3?source=SEED&type=organism&identity=60&length=15" -o mgm4675923.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685116.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685116.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685117.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685117.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685127.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685127.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685128.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685128.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685129.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685129.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685130.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685130.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4673326.3?source=SEED&type=organism&identity=60&length=15" -o mgm4673326.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685152.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685152.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685153.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685153.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685154.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685154.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685155.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685155.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685156.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685156.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685157.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685157.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685158.3?source=SEED&type=organism&identity=60&length=15" -o mgm4685158.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4690995.3?source=SEED&type=organism&identity=60&length=15" -o mgm4690995.3.seed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4690996.3?source=SEED&type=organism&identity=60&length=15" -o mgm4690996.3.seed
+
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685151.3?source=SEED&type=function&identity=60&length=15" -o mgm4685151.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4673324.3?source=SEED&type=function&identity=60&length=15" -o mgm4673324.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4675922.3?source=SEED&type=function&identity=60&length=15" -o mgm4675922.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4675923.3?source=SEED&type=function&identity=60&length=15" -o mgm4675923.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685116.3?source=SEED&type=function&identity=60&length=15" -o mgm4685116.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685117.3?source=SEED&type=function&identity=60&length=15" -o mgm4685117.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685127.3?source=SEED&type=function&identity=60&length=15" -o mgm4685127.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685128.3?source=SEED&type=function&identity=60&length=15" -o mgm4685128.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685129.3?source=SEED&type=function&identity=60&length=15" -o mgm4685129.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685130.3?source=SEED&type=function&identity=60&length=15" -o mgm4685130.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4673326.3?source=SEED&type=function&identity=60&length=15" -o mgm4673326.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685152.3?source=SEED&type=function&identity=60&length=15" -o mgm4685152.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685153.3?source=SEED&type=function&identity=60&length=15" -o mgm4685153.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685154.3?source=SEED&type=function&identity=60&length=15" -o mgm4685154.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685155.3?source=SEED&type=function&identity=60&length=15" -o mgm4685155.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685156.3?source=SEED&type=function&identity=60&length=15" -o mgm4685156.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685157.3?source=SEED&type=function&identity=60&length=15" -o mgm4685157.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4685158.3?source=SEED&type=function&identity=60&length=15" -o mgm4685158.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4690995.3?source=SEED&type=function&identity=60&length=15" -o mgm4690995.3.fseed
+curl  -H "auth: ibv5dnQkteDnpbsWU5YJj3JKH" -H 'Accept-Encoding: gzip,deflate' "http://api.metagenomics.anl.gov/1/annotation/similarity/mgm4690996.3?source=SEED&type=function&identity=60&length=15" -o mgm4690996.3.fseed
+
+
+
